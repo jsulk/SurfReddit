9e563735c8d98565c74d6548ae8a8912f1c582fb
Jake Sulkoske <jakesulkoske@Jakes-MBP.lan1>
2020-02-05 15:21:36 -0800

    Initial commit

---
+++ SurfReddit/Models/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  SurfReddit
+//
+//  Created by Jake Sulkoske on 2/5/20.
+//  Copyright © 2020 Jake Sulkoske. All rights reserved.
+//
+
+import Foundation
+
+struct Response: Decodable {
+    var data: Data
+}
+
+struct Data: Decodable {
+    
+}
