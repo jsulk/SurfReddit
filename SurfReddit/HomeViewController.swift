128d33d2c710e4e26b71f19b7837525436bc437c
Jake Sulkoske <jakesulkoske@Jakes-MBP.lan1>
2020-02-05 16:00:25 -0800

    Added Navigation bar. Fixed Separator inset. Added Mark for HomeVC

--- SurfReddit/HomeViewController.swift
+++ SurfReddit/HomeViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PostData]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         getPosts()
@@ -21,17 +25,34 @@ class HomeViewController: UIViewController {
             if let error = error {
                 print(error)
             } else {
-                guard let posts = posts else {
-                    print("NO")
-                    return
+                guard let posts = posts else { return }
+                for post in posts {
+                    self.posts.append(post.data)
                 }
-                print(posts)
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             }
         }
     }
     
 }
 
+// MARK: UITableViewDataSource methods
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath)
+        cell.textLabel?.text = posts[indexPath.row].title
+        return cell
+    }
     
 }
